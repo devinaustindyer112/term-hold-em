b080590499b00fb2ebd572c6e84a10234c6b2d16
Devin Dyer <devin.dyer@efwnow.com>
2025-03-18 11:03:30 -0400

    update game header

--- src/game.h
+++ src/game.h
@@ -4,13 +4,10 @@
 typedef struct { 
     // TODO: Add the ability for different game modes. 
     int player_count;
-    Player players[10]
+    Player players[9]
     float big_blind;
     float small_blind;
-    float buy_in;
     float pot;
 } Game;
-
-// Define the functions we will need first
 
 #endif // GAME_H
